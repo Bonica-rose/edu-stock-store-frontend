c90383de72957d49fe4ae64c39644e83fbe914ed
Bonica-rose <eveningstarforever@gmail.com>
2026-08-01 18:55:32 +0530

    authentication basic files created

---
+++ src/shared/api/apiEndpoints.js
@@ -0,0 +1,12 @@
+const API_ENDPOINTS = {
+    AUTH: {
+        LOGIN: "/auth/login",
+        LOGOUT: "/auth/logout",
+        CURRENT_USER: "/auth/me",
+        CHANGE_PASSWORD: "/auth/change-password",
+        FORGOT_PASSWORD: "/auth/forgot-password",
+        RESET_PASSWORD: "/auth/reset-password",
+    },
+};
+
+export default API_ENDPOINTS;
